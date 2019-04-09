e109dbc93dff030838c88edd4d94695daf2a419f
Rodrigo Carneiro <rodrigo_carneiro@ymail.com>
2019-04-09 19:01:28 +0100

    Add phpunit tests and dependencies

---
+++ api/tests/unit/example_test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample() {
+        $this->assertEquals(1, 1);
+    }
+}
