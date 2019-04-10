6db8d9d06a73bc26b04cbf31fa7fed555eca6e89
Rodrigo Carneiro <rodrigo_carneiro@ymail.com>
2019-04-10 18:01:29 +0100

    Add a new unit test example

--- api/tests/unit/01_ExampleTest.php
+++ api/tests/unit/01_ExampleTest.php
@@ -9,4 +9,19 @@ class ExampleTest extends TestCase
     public function testExample() {
         $this->assertEquals(1, 1);
     }
+
+    /**
+     * @dataProvider someProvider
+     */
+    public function testExampleWithProvider($expected, $a, $b) {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function someProvider() {
+        return [
+            "One plus One"      => [2, 1, 1],
+            "One plus zero"     => [10, 1, 9],
+            "Zero plus zero"    => [0, 0, 0]
+        ];
+    }
 }
