539ac937389fc3d2876b620487b927521a3eaefb
Rodrigo Carneiro <rodrigo_carneiro@ymail.com>
2019-12-29 21:43:16 +0000

    Updates for the first execution

--- api/startup_script.sh
+++ api/startup_script.sh
@@ -3,12 +3,11 @@ set -e
 
 CONTAINER_ALREADY_STARTED="var/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
     make install
     make setup
-    make test
-    make fixture
+    make fixtures
+    touch $CONTAINER_ALREADY_STARTED
 else
     echo "-- Not first container startup --"
 fi
